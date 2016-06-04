1e9168ce32824a3bbe3c2fc0abf358c27500a512
corywmoore <cory@abovethenoi.se>
2016-06-03 19:51:26 -0500

    Authentication
    Login works. Needs to be wired to database with EF6 code first

---
+++ AngularWebAPI.WebAPI/Controllers/SecureDataController.cs
@@ -0,0 +1,21 @@
+﻿using AngularWebAPI.WebAPI.Models.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace AngularWebAPI.WebAPI.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [Authorize]
+    public class SecureDataController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok(new { secureData = "You have to be authenticated to access this!" });
+        }
+    }
+}
